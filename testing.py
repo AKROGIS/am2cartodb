e1cb03a76f5c2e672cb58ebe15cef4204a5ffa18
Regan Sarwas <Regan_Sarwas@nps.gov>
2021-02-02 17:36:19 -0900

    Fix the tests to work with the NPS Server
    It appears the hosted acount has been set to read only.

--- testing.py
+++ testing.py
@@ -3,29 +3,51 @@
 Simple tests of the Carto (SQL based mapping database) API.
 
 You must have a Carto (https://carto.com) (formerly Cartodb) account and
-apikey - these are set in the secrets.py file.  Note that the apikey is not
-required for select statements on a public table.
+apikey - these are set in the `carto_secrets.py` file.  Note that the apikey is
+not required for select statements on a public table.  This code will work with
+an hosted server on carto.com, or an on premises installation. Modify the
+base_url value in the Config object as needed.
 
-The Carto module provides a connection that acts like a database server e.i. it
-takes an SQL statement and returns a list of rows by wraps a ReST request and
+The Carto module provides a connection that acts like a database server i.e. it
+takes an SQL statement and returns a list of rows by wrapping a ReST request and
 response.
 
 Example SQL request in the browser:
-  domain = {domain} aka username
-  apikey = {apikey}
-  sql = {sql}
-  https://{domain}.carto.com/api/v2/sql?api_key={apikey}&q={sql}
+  Hosted:
+  https://{user}.carto.com/api/v2/sql?api_key={apikey}&q={sql}
+  On Premises
+  https://{domain}/user/{user}/api/v2/sql?api_key={apikey}&q={sql}
+
+Carto references (2021-02-01):
+https://github.com/CartoDB/carto-python
+https://carto.com/developers/python-sdk/
+https://carto-python.readthedocs.io/en/latest/carto.html#module-carto.sql
+
+If the on-premises carto server is using a self signed certificate, or if
+the Python `certifi` module does not have all of the necessary certificates
+to verify the server (happed with the nps server in 2021), then you will
+need to use the carto.auth.NonVerifiedAPIKeyAuthClient authorization,
+and/or the request with `verify=False` (see the commented lines in the code).
+I was able to add the necessary certificates by checking with the browser.
+In Chrome visit the server and click on the lock icon, and find information
+on the certificate. Click the download certificate chain as PEM, and add it to
+the end of the file `cacert.pem` in the `certifi` module in the Python
+`site-packages`.
 
 Third party requirements:
-* cartodb - https://pypi.python.org/pypi/cartodb
-  (Note: This module is written for Python 2.5 and does not work with 3.x)
+* carto - https://pypi.python.org/pypi/carto
+  (for large and/or authenticated requests)
 * requests - https://pypi.python.org/pypi/requests
   (for simple non-authenticated GET requests)
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-# from cartodb import CartoDBAPIKey, CartoDBException
+from carto.auth import APIKeyAuthClient
+
+# To disable SSL certificate verification (unsafe)
+# from carto.auth import NonVerifiedAPIKeyAuthClient
+from carto.sql import SQLClient, CartoException
 import requests
 
 import carto_secrets
@@ -36,9 +58,14 @@ class Config(object):
 
     # pylint: disable=useless-object-inheritance,too-few-public-methods
 
-    base_url = "http://{user}.carto.com/api/v2/sql/".format(
-        user=carto_secrets.domain
-    )
+    # On premises read/write
+    base_url = "https://carto.nps.gov/user/{user}/".format(user=carto_secrets.user)
+    # Hosted read
+    # base_url = "https://{user}.carto.com/"
+    # base_url = "https://nationalparkservice.carto.com/user/{user}/"
+
+    # For public GET requests
+    sql_url = base_url + "api/v2/sql/"
 
     # A look up table of testing/configuration queries.
     # The first X queries create and modify new test tables. They have the
@@ -70,11 +97,11 @@ class Config(object):
         """,
         # Add spatial columns (cartod-ify) to the locations table
         3: "SELECT cdb_cartodbfytable('{0}', 'Animal_Locations5')".format(
-            carto_secrets.domain
+            carto_secrets.user
         ),
         # Add spatial columns (cartod-ify) to the locations table
         4: "SELECT cdb_cartodbfytable('{0}', 'Animal_Movements5')".format(
-            carto_secrets.domain
+            carto_secrets.user
         ),
         # Create a new Animal Location record
         5: """
@@ -100,7 +127,7 @@ class Config(object):
                 24.0,
                 12.345,
                 ST_SetSRID(
-                    ST_GeomFromText('LINESTRING(-153.381, 58.817, -153.480, 58.765)'),
+                    ST_GeomFromText('LINESTRING(-153.381 58.817, -153.480 58.765)'),
                     4326
                 )
             )
@@ -121,9 +148,7 @@ class Config(object):
         9: "DROP TABLE Animal_Locations5",
         # Delete the Animal Movements table
         10: "DROP TABLE Animal_Movements5",
-
         # Read only public access queries
-
         # Get a count of the records in the Animal Locations table
         11: "SELECT count(*) FROM Animal_Locations",
         # Get the top 10 Location records in database creation order.
@@ -155,41 +180,74 @@ class Config(object):
     }
 
 
-def get_carto_connection():
-    """Return a connection to the carto database, using the secrets."""
+def get_auth_carto_sql_connection():
+    """Return a authorized SQL connection to the carto database, using the secrets."""
 
-    return CartoDBAPIKey(carto_secrets.apikey, carto_secrets.domain)
+    # To disable SSL certificate verification (unsafe)
+    # auth_client = NonVerifiedAPIKeyAuthClient(
+    auth_client = APIKeyAuthClient(
+        api_key=carto_secrets.apikey,
+        base_url=Config.base_url,
+    )
+    return SQLClient(auth_client)
 
 
-def test(query):
+def auth_query(query):
     """Run a test query and print the results (or error)."""
 
-    connection = get_carto_connection()
+    connection = get_auth_carto_sql_connection()
+    print(query)
     try:
-        result = connection.sql(query)
-        print(result)
-    except CartoDBException as ex:
-        print("Some error ocurred {0}".format(ex))
+        data = connection.send(query)
+        print(data)
+    except CartoException as ex:
+        print("some error ocurred {0}".format(ex))
+
+
+def auth_queries():
+    """Run all authenticated test queries and print the results (or error)."""
+
+    for i in range(1, 11):
+        print("\nTest #{0}: ".format(i), end="")
+        auth_query(Config.queries[i])
 
 
 def public_query(query):
     """Run a public (non-authenticated test query and print the results (or error)."""
 
-    url = "{0}?q={1}".format(Config.base_url, query)
+    url = "{0}?q={1}".format(Config.sql_url, query)
     print(url)
     try:
+        # To disable SSL certificate verification (unsafe)
+        # result = requests.get(url, verify=False)
         result = requests.get(url)
         print(result.json())
     except requests.exceptions.RequestException as ex:
         print("Some error ocurred {0}".format(ex))
 
-def public_tests():
-    """Run all public (non-authenticated test queries and print the results (or error)."""
+
+def public_queries():
+    """Run all public (non-authenticated) test queries and print the results (or error)."""
 
     for i in range(11, 19):
         print("\nTest #{0}: ".format(i), end="")
         public_query(Config.queries[i])
 
 
-# test(Config.queries[15])
-public_tests()
+# Run a single adhoc public query
+# public_query("select count(*) from animal_locations")
+
+# Run a single adhoc authenticated query
+# auth_query("select count(*) from animal_locations")
+
+# Run a single predefined public query
+# public_query(Config.queries[15])
+
+# Run a single authenticated query
+# auth_query(Config.queries[1])
+
+# Run all public query
+# public_queries()
+
+# Run all authenticated query
+# auth_queries()
